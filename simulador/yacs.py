c2cbdc1e1ccafbecc53ab4286ad790f33c93c07e
SwTzu <morbemon2012@gmail.com>
2024-05-30 13:56:05 -0400

    cositas

--- simulador/yacs.py
+++ simulador/yacs.py
@@ -2,7 +2,8 @@
 
 import simpy
 import argparse
-
+import numpy as np
+import string
 class Debug(object):
     @classmethod
     def log(self, env: simpy.Environment, msg: str):
@@ -53,7 +54,7 @@ class Parametros(object):
         self.procesos = args.procesos
         self.cores = args.cores
         try:
-            if args.L1 > args.L2:
+            if args.L1 >= args.L2:
                 raise argparse.ArgumentTypeError(f"La memoria L1 debe ser menor a la memoria L2")
             else:
                 self.memoriaL1 = args.L1
@@ -67,8 +68,26 @@ class Proceso(object):
     def __init__(self, env: simpy.Environment, idProceso: int):
         self.env = env
         self.idProceso = idProceso
+        self.num_datos= np.random.randint(1, 25)
+        self.datos= [
+            [letra,False] for letra in np.random.choice(list(string.ascii_lowercase), self.num_datos, replace=False)]
 
-        
+class Core(object):
+    def __init__(self, idProceso:int, L1:int, L2:int):
+        self.idProceso = idProceso
+        self.L1 = L1
+        self.L2 = L2
+        self.procesosL1=[]
+        self.procesosL2=[]
+        self.Tcore=[]
+        self.Tused=0
+        self.prcsSolved=0
+        self.cL1=0
+        self.cL2=0
+        self.ram=0
+        self.TL1 = 4
+        self.TL2 = 10
+        self.inUse = False
 
 class Procesador(object):
 
@@ -82,9 +101,46 @@ class Procesador(object):
         procesadorSim = self.env.process(self.run())
 
     def run(self):
-        for idProceso in range(0, self.procesos):
-            self.env.process(self.proceso(idProceso))
+        for idProceso in range(self.procesos):
+            self.env.process(Proceso(idProceso))
 
+    def use_data(self,core:Core,data):
+        if len(core.procesosL1)>core.L1:
+            core.procesosL1.pop(0)
+        if len(core.procesosL2)>core.L2:
+            core.procesosL2.pop(0)
+        if data in core.procesosL1:
+            core.cL1+=1
+            yield core.TL1
+        elif data in core.procesosL2:
+            core.cL2+=1
+            yield core.TL2 + core.TL1
+        else:
+            core.procesosL2.append(data)
+            core.procesosL1.append(data)
+            core.ram+=1
+            yield core.TL2 + core.TL1 + core.ram
+
+    def assign(self):
+        for i in self.cores:
+            if not i.inUse:
+                i.inUse=True
+                return i
+
+    def succes(self,core:Core):
+        core.inUse=False
+
+class PC(object):
+    def __init__(self, env: simpy.Environment, cores:int, L1:int, L2:int, procesos:int):
+        self.env = env
+        self.procesos=procesos
+        self.Psuccess=0
+        self.ram=200
+        self.freecore=simpy.Container(env,cores,init=cores)
+        self.cores=[Core(i,L1,L2) for i in range(cores)]
+        self.env.process(self.run())
+
+    
 
 def main():
 
